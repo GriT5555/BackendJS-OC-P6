21cbdd7bc4823334c9f977157656f1c841009d0c
Mat <113144372+GriT5555@users.noreply.github.com>
2026-05-18 01:31:18 +0200

    Future routes

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,16 @@ async function run() {
 }
 run().catch(console.dir);
 
+/*app.post('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new book({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});*/
+
 /* app.get('/api/books', (req, res, next) => {
   const stuff = [
     {
@@ -56,11 +66,16 @@ run().catch(console.dir);
   res.status(200).json(stuff);
 }); */ 
 
-/*app.post('/api/books', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
-  });
+/*app.use('/api/books', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});*/
+
+/*app.get('/api/books/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
 });*/
 
 module.exports = app;
