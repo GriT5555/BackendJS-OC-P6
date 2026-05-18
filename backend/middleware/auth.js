c071921175d5501e0324f49be7a278f2e92329c5
Mat <113144372+GriT5555@users.noreply.github.com>
2026-05-18 13:16:15 +0200

    Auth middleware
    + router link

---
+++ backend/middleware/auth.js
@@ -0,0 +1,14 @@
+const JWT = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const clearToken = JWT.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = clearToken.userId;
+        req.auth = {
+            userId: userId
+        }
+    } catch(error) {
+        res.status(401).json({error});
+    }
+};
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const Book = require('../models/books');
 const router = express.Router();
 const booksCtrl = require('../controllers/controls');
+const auth = require('../middleware/auth');
 
-router.post('/', booksCtrl.createBook);
-router.put('/:id', booksCtrl.updateBook);
-router.delete('/:id', booksCtrl.deleteBook);
-router.get('/:id', booksCtrl.oneBook);
-router.get('/' +'', booksCtrl.everyBook);
+
+router.post('/', auth, booksCtrl.createBook);
+router.put('/:id', auth, booksCtrl.updateBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.get('/:id', auth, booksCtrl.oneBook);
+router.get('/' +'', auth, booksCtrl.everyBook);
 
 module.exports = router;
