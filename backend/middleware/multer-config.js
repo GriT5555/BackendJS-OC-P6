295c6a2b8d6990c42bdf2bae288a8a8eb9a4b98f
Mat <113144372+GriT5555@users.noreply.github.com>
2026-05-27 10:50:13 +0200

    Multer config

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,20 @@
+const multer = requre ("multer");
+
+const MIME_TYPES = {
+    'images/jpg':'jpg',
+    'images/jpeg':'jpeg',
+    'images/png':'png'
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'books')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES(file.mimetype);
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({ storage })
